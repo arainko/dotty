7a4bfe6f6963e8f4b07d64349ab3ae6888f03078
arainko <aleksander.rainko99@gmail.com>
2024-11-20 19:44:09 +0100

    add a test with YesNoEnum

--- tests/pos/as-in-derives/Test.scala
+++ tests/pos/as-in-derives/Test.scala
@@ -1,19 +1,73 @@
-trait Typeclass[A] {
-  def someValue: String
+import scala.deriving.*
+
+import scala.compiletime.*
+
+// =============== Typeclass definition
+trait YesNoEnum[A] {
+  extension (self: A) def toBoolean: Boolean
+
+  def apply(bool: Boolean): A
 }
 
-object Typeclass {
-  def derived[A]: Typeclass[A] = new {
-    def someValue: String = "someValue"
-  }
+object YesNoEnum {
+
+  inline def derived[A <: scala.reflect.Enum](using
+    A: Mirror.SumOf[A],
+    ev1: A.MirroredElemLabels <:< ("Yes", "No")
+  ): YesNoEnum[A] =
+    new {
+      private val instances = summonInstances[A.MirroredElemTypes, A]
+      private val yes = instances(0)
+      private val no = instances(1)
+
+      extension (self: A) def toBoolean: Boolean = A.ordinal(self) == 0
+
+      def apply(bool: Boolean): A =
+        if bool then yes else no
+      
+    }
+
+  private inline def summonInstances[A <: Tuple, Parent <: scala.reflect.Enum]: List[Parent] =
+    inline erasedValue[A] match {
+      case _: (h *: t) =>
+        inline valueOf[h] match {
+          case parent: Parent => parent :: summonInstances[t, Parent]
+        }
+
+      case _: EmptyTuple => Nil
+    }
 }
 
-case class Test(a: Int, b: Int) derives Typeclass as costam, Typeclass as costam2
 
-object Test {
-  export costam.someValue
+// =============== usage (current) ===============
+
+enum DecisionCurrent derives YesNoEnum {
+  case Yes, No
 }
 
-@main def main = {
-  println(Test.someValue)
+object DecisionCurrent {
+  export derived$YesNoEnum.{apply as fromBoolean}
+}
+
+
+// =============== usage (new) ===============
+
+enum Decision derives YesNoEnum as fromBoolean  {
+  case Yes, No
+}
+
+enum DecisionAlternate derives YesNoEnum as yesNoEnum {
+  case Yes, No
+}
+
+object DecisionAlternate {
+  export yesNoEnum.{apply as fromBoolean}
+}
+
+@main def test = {
+  Decision.fromBoolean(true) // Yes
+  Decision.fromBoolean(false) // No
+
+  DecisionAlternate.fromBoolean(true) // Yes
+  DecisionAlternate.fromBoolean(false) // No
 }
